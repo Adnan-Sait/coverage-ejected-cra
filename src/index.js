8367a1b7caee7a390181e403ee823e9eb2499314
Adnan Sait <adnansait74@gmail.com>
2023-07-30 12:49:17 +0530

    React version updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
