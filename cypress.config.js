1c10e83566c5d4afe168d23f4d937b86c2c8e45e
Adnan Sait <adnansait74@gmail.com>
2023-08-12 17:37:39 +0530

    Updated Cypress config to omit artifacts
    Disabled Video and Screenshot creaction
    Set base URL for E2E Testing

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://localhost:3000/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: false,
+    screenshotOnRunFailure: false,
   },
 
   component: {
@@ -12,5 +15,7 @@ module.exports = defineConfig({
       framework: "create-react-app",
       bundler: "webpack",
     },
+    video: false,
+    screenshotOnRunFailure: false,
   },
 });
